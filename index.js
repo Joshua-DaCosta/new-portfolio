994dddc0b87a6c8c8f9a35eba125206b1b15f83b
Joshua Da Costa <Joshua.0331@hotmail.com>
2022-05-04 19:34:08 -0400

    added an alet on submit

---
+++ index.js
@@ -0,0 +1,14 @@
+const submit = () => {
+    const name = 
+    window.alert('thank you for reaching out I will get back to you shortly');
+}
+
+const submitButton = document.getElementById('submit-btn');
+
+submitButton.addEventListener('click', submit);
+
+const formElem = document.querySelector("form");
+
+formElem.addEventListener("submit", (e) => {
+
+});
