64c3422308a5f3a494d86e6c80bdea7d12b8037e
Joshua Da Costa <Joshua.0331@hotmail.com>
2022-05-04 19:35:15 -0400

    remove uneeded code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const submit = () => {
-    const name = 
+    
     window.alert('thank you for reaching out I will get back to you shortly');
 }
 
@@ -7,8 +7,3 @@ const submitButton = document.getElementById('submit-btn');
 
 submitButton.addEventListener('click', submit);
 
-const formElem = document.querySelector("form");
-
-formElem.addEventListener("submit", (e) => {
-
-});
